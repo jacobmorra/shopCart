8f3a72751eb93bfbd868ce02209f0a9f6bf3c849
jacobmorra <jacob.morra@uoit.net>
2015-11-13 02:20:48 -0500

    allow  for user upload into database

--- regscript.php
+++ regscript.php
@@ -1,7 +1,21 @@
 <?php
+//if (isset($_POST["submit"]) && !empty($_POST["uname"]) && !empty($_POST["upass"]) && !empty($_POST["umail"])){
 	$dbLocalhost = mysql_connect("localhost:3306", "root", "")
 			or die("Could not connect: " . mysql_error());
 	
 	mysql_select_db("shopCartUsers", $dbLocalhost)
 			or die ("Could not find database: " . mysql_error());
+
+	$insertRec = "INSERT INTO userinfo (username, password, email) 
+		VALUES ('$_POST[uname]', '$_POST[upass]', '$_POST[umail]')";
+		
+	mysql_query($insertRec, $dbLocalhost)
+		or die("Could not insert user: " . mysql_error());
+	
+	echo "<h3>Welcome to our site, '$_POST[uname]'!</h3>";
+//}
+//else
+//{
+//	echo "something went wrong..";
+//}
 ?>
